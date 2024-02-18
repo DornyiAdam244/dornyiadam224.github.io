12e63562d2870a358176fc19729e4d9584a9fe06
Dornyi Adam Robert <94841029+DornyiAdam244@users.noreply.github.com>
2024-02-18 20:10:25 +0100

    Update

---
+++ index (2)/js/custom.js
@@ -0,0 +1,31 @@
+(() => {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+      form.addEventListener('submit', event => {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+  
+        form.classList.add('was-validated')
+      }, false)
+    })
+  })()
+
+  function validateForm(event) {
+    var form = document.querySelector('.needs-validation');
+
+    if (form.checkValidity() === false) {
+        event.preventDefault();
+        event.stopPropagation();
+    } else {
+        alert('Adatait fogadtuk!');
+    }
+
+    form.classList.add('was-validated');
+}
